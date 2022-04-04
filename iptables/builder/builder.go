007d31b422632bf97449c1ff9d84d4b2dfbb8cdd
Bart Smykla <bartek@smykla.com>
2022-04-04 13:06:07 +0200

    feat: add logic to exec iptables-resolve (#17)
    Signed-off-by: Bart Smykla <bartek@smykla.com>

--- iptables/builder/builder.go
+++ iptables/builder/builder.go
@@ -1,8 +1,12 @@
 package builder
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"os/exec"
 	"strings"
+	"time"
 
 	"github.com/kumahq/kuma-net/iptables/config"
 	"github.com/kumahq/kuma-net/iptables/table"
@@ -33,11 +37,12 @@ func (t *IPTables) Build(verbose bool) string {
 		tables = append(tables, nat)
 	}
 
+	separator := "\n"
 	if verbose {
-		return strings.Join(tables, "\n\n")
+		separator = "\n\n"
 	}
 
-	return strings.Join(tables, "\n")
+	return strings.Join(tables, separator) + "\n"
 }
 
 func BuildIPTables(config *config.Config) (string, error) {
@@ -50,4 +55,42 @@ func BuildIPTables(config *config.Config) (string, error) {
 		buildRawTable(config),
 		buildNatTable(config, loopbackIface.Name),
 	).Build(config.Verbose), nil
+}
+
+func saveIPTablesRestoreFile(f *os.File, content string) error {
+	defer f.Close()
+
+	fmt.Println("Writing following contents to rules file: ", f.Name())
+	fmt.Println(content)
+
+	writer := bufio.NewWriter(f)
+	_, err := writer.WriteString(content)
+	if err != nil {
+		return fmt.Errorf("unable to write iptables-restore file: %s", err)
+	}
+
+	return writer.Flush()
+}
+
+func RestoreIPTables(config *config.Config) (string, error) {
+	filename := fmt.Sprintf("iptables-rules-%d.txt", time.Now().UnixNano())
+	rulesFile, err := os.CreateTemp("", filename)
+	if err != nil {
+		return "", fmt.Errorf("unable to create iptables-restore file: %s", err)
+	}
+	defer os.Remove(rulesFile.Name())
+
+	rules, err := BuildIPTables(config)
+	if err != nil {
+		return "", fmt.Errorf("unable to build iptable rules: %s", err)
+	}
+
+	if err := saveIPTablesRestoreFile(rulesFile, rules); err != nil {
+		return "", err
+	}
+
+	cmd := exec.Command("iptables-restore", "--noflush", rulesFile.Name())
+	output, err := cmd.CombinedOutput()
+
+	return string(output), err
 }
